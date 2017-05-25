502c75715623b9e39ee00e3b0f42919ef81e9ed7
AndropaX <kpax.mar@gmail.com>
2017-05-25 16:26:41 +0200

    build charger packages

--- product/platform.mk
+++ product/platform.mk
@@ -7,6 +7,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     usf_post_boot.sh
 
+# for off charging mode
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    libhealthd.qcom
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
